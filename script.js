c47e920eb9b2542c00e631ff326e176ffa23c4d6
bansirajodiya <bansirajodiya22@gmail.com>
2024-06-07 12:17:31 +0530

    changed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-burgerIcon = document.querySelector("navbar-burger");
-navMenu = document.getElementById("Example");
+const burgerIcon = document.querySelector(".navbar-burger");
+const navMenu = document.getElementById("Example");
 burgerIcon.addEventListener("click", () => {
 	burgerIcon.classList.toggle("is-active");
 	navMenu.classList.toggle("is-active");
